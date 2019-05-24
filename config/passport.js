7de0ebf15a8eec2d0445a70fda45689113a0495f
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-05-24 17:37:00 -0500

    Users saving to MongoDB database via Passport

--- config/passport.js
+++ config/passport.js
@@ -1,14 +1,27 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const db = require('../models');
+
+const { User } = db;
 
 passport.use(
-  new GoogleStrategy({
-    // options for the strategy
-    callbackURL: '/auth/google/redirect',
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  },
-  (accessToken, refreshToken, profile, done) => {
-    console.log(profile);
-  }),
+  new GoogleStrategy(
+    {
+      // options for the strategy
+      callbackURL: '/auth/google/redirect',
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // console.log(profile);
+      new User({
+        username: profile.displayName,
+        googleId: profile.id,
+      })
+        .save()
+        .then((newUser) => {
+          console.log(`User added! Details: ${newUser}`);
+        });
+    },
+  ),
 );
