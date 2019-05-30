46fe44b1d7b0fff1fca08afcb92ea42ac3ebd0f2
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-05-30 18:54:33 -0500

    Articles successfully populating index.hbs page

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
-axios.get('/data/articleScrape').then(async (response) => {
+// HTML Variables
+
+const main = document.querySelector('main');
+
+// Event Listener Functions
+
+function getComments() {
+  const { id } = this.dataset;
+  axios.get(`/data/comments/${id}`).then((response) => {
+    const { comments } = response.data[0];
+    if (comments.length !== 0) {
+      console.log(comments);
+    }
+  });
+}
+
+// Page-Load Calls
+
+axios.get('/data/articleScrape').then((response) => {
   console.log(response);
   setTimeout(getArticles, 1500);
 });
@@ -6,5 +24,30 @@ axios.get('/data/articleScrape').then(async (response) => {
 function getArticles() {
   axios.get('/data/articles').then((response) => {
     console.log(response.data);
+
+    const articles = response.data;
+
+    articles.forEach((article) => {
+      const newArticleDiv = document.createElement('div');
+      const newArticleImg = document.createElement('img');
+      const newArticleTitle = document.createElement('h3');
+      const newArticleWebsite = document.createElement('h6');
+      const newArticleLink = document.createElement('a');
+      const newCommentsBtn = document.createElement('button');
+      newArticleDiv.classList.add('article');
+      newArticleImg.src = article.imgURL;
+      newArticleTitle.textContent = article.title;
+      newArticleWebsite.textContent = article.siteURL;
+      newCommentsBtn.textContent = 'Comments';
+      newCommentsBtn.setAttribute('data-id', article._id);
+      newCommentsBtn.addEventListener('click', getComments);
+      newArticleLink.setAttribute('href', article.linkURL);
+      newArticleLink.appendChild(newArticleTitle);
+      newArticleDiv.appendChild(newArticleLink);
+      newArticleDiv.appendChild(newArticleImg);
+      newArticleDiv.appendChild(newArticleWebsite);
+      newArticleDiv.appendChild(newCommentsBtn);
+      main.appendChild(newArticleDiv);
+    });
   });
 }
