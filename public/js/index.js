4f5588dc8d81a4610537eae4a942873da2d5a383
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-05-31 18:59:28 -0500

    Anonymous comments from index.js posting successfully on front end

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,20 @@ const main = document.querySelector('main');
 
 // Event Listener Functions
 
+function postComment(e) {
+  e.preventDefault();
+  const { id } = this.dataset;
+  const parentDiv = this.parentNode;
+  const data = {
+    username: 'Anon',
+    message: parentDiv.querySelector('textarea').value.trim(),
+    article_id: id,
+  };
+  axios.post('/data/comment', data).then((response) => {
+    console.log(response);
+  });
+}
+
 function getComments() {
   const { id } = this.dataset;
   const articleDiv = this.parentNode;
@@ -35,24 +49,23 @@ function getComments() {
       const commentTextArea = document.createElement('textarea');
       const submitBtn = document.createElement('button');
       submitBtn.textContent = 'Comment';
+      submitBtn.setAttribute('data-id', id);
+      submitBtn.addEventListener('click', postComment);
       commentsDiv.appendChild(commentTextArea);
       commentsDiv.appendChild(submitBtn);
       articleDiv.appendChild(commentsDiv);
     } else {
-      const commentsDiv = document.createElement('div');
       const commentTextArea = document.createElement('textarea');
       const submitBtn = document.createElement('button');
       commentsDiv.classList.add('comments-div');
       submitBtn.textContent = 'Comment';
+      submitBtn.setAttribute('data-id', id);
+      submitBtn.addEventListener('click', postComment);
       commentsDiv.appendChild(commentTextArea);
       commentsDiv.appendChild(submitBtn);
       articleDiv.appendChild(commentsDiv);
     }
   });
-}
-
-function postComment() {
-    
 }
 
 // Page-Load Calls
