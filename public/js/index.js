c12a264ccfee8a09cc055c9bc083fab084ec34b1
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-05-31 17:48:20 -0500

    Comment pop-up window working

--- public/js/index.js
+++ public/js/index.js
@@ -6,10 +6,20 @@ const main = document.querySelector('main');
 
 function getComments() {
   const { id } = this.dataset;
+  const articleDiv = this.parentNode;
   axios.get(`/data/comments/${id}`).then((response) => {
     const { comments } = response.data[0];
     if (comments.length !== 0) {
       console.log(comments);
+    } else {
+      const commentsDiv = document.createElement('div');
+      const commentTextArea = document.createElement('textarea');
+      const submitBtn = document.createElement('button');
+      commentsDiv.classList.add('comments-div');
+      submitBtn.textContent = 'Comment';
+      commentsDiv.appendChild(commentTextArea);
+      commentsDiv.appendChild(submitBtn);
+      articleDiv.appendChild(commentsDiv);
     }
   });
 }
@@ -18,36 +28,33 @@ function getComments() {
 
 axios.get('/data/articleScrape').then((response) => {
   console.log(response);
-  setTimeout(getArticles, 1500);
 });
 
-function getArticles() {
-  axios.get('/data/articles').then((response) => {
-    console.log(response.data);
+axios.get('/data/articles').then((response) => {
+  console.log(response.data);
 
-    const articles = response.data;
+  const articles = response.data;
 
-    articles.forEach((article) => {
-      const newArticleDiv = document.createElement('div');
-      const newArticleImg = document.createElement('img');
-      const newArticleTitle = document.createElement('h3');
-      const newArticleWebsite = document.createElement('h6');
-      const newArticleLink = document.createElement('a');
-      const newCommentsBtn = document.createElement('button');
-      newArticleDiv.classList.add('article');
-      newArticleImg.src = article.imgURL;
-      newArticleTitle.textContent = article.title;
-      newArticleWebsite.textContent = article.siteURL;
-      newCommentsBtn.textContent = 'Comments';
-      newCommentsBtn.setAttribute('data-id', article._id);
-      newCommentsBtn.addEventListener('click', getComments);
-      newArticleLink.setAttribute('href', article.linkURL);
-      newArticleLink.appendChild(newArticleTitle);
-      newArticleDiv.appendChild(newArticleLink);
-      newArticleDiv.appendChild(newArticleImg);
-      newArticleDiv.appendChild(newArticleWebsite);
-      newArticleDiv.appendChild(newCommentsBtn);
-      main.appendChild(newArticleDiv);
-    });
+  articles.forEach((article) => {
+    const newArticleDiv = document.createElement('div');
+    const newArticleImg = document.createElement('img');
+    const newArticleTitle = document.createElement('h3');
+    const newArticleWebsite = document.createElement('h6');
+    const newArticleLink = document.createElement('a');
+    const newCommentsBtn = document.createElement('button');
+    newArticleDiv.classList.add('article');
+    newArticleImg.src = article.imgURL;
+    newArticleTitle.textContent = article.title;
+    newArticleWebsite.textContent = article.siteURL;
+    newCommentsBtn.textContent = 'Comments';
+    newCommentsBtn.setAttribute('data-id', article._id);
+    newCommentsBtn.addEventListener('click', getComments);
+    newArticleLink.setAttribute('href', article.linkURL);
+    newArticleLink.appendChild(newArticleTitle);
+    newArticleDiv.appendChild(newArticleLink);
+    newArticleDiv.appendChild(newArticleImg);
+    newArticleDiv.appendChild(newArticleWebsite);
+    newArticleDiv.appendChild(newCommentsBtn);
+    main.appendChild(newArticleDiv);
   });
-}
+});
