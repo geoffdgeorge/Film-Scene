9c40fc3abadf88d3b05dc21df31ee83480e58882
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-06-02 15:12:58 -0500

    Heroku push to test Google authentication'

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').OAuth2Strategy;
+const GoogleStrategy = require('passport-google-oauth20');
 const TwitterStrategy = require('passport-twitter').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const db = require('../models');
